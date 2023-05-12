0dd888467827b5d3a905475d321b0acb56284480
markgregg <gregg.mark@gmail.com>
2023-05-12 18:52:09 +0100

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-//import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-//import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './state/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import { registerUmami } from 'pwa-synergy-umami';
+import { register, reportWebVitals } from 'pwa-synergy-api';
 
+registerUmami('http://umami.com:3000','4867c726-dcb5-47a6-a087-ccc285fd315d');
 const  persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(
@@ -28,9 +29,10 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-//serviceWorkerRegistration.register();
+register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+reportWebVitals();
+
